870332c8a404d0b6f9e4bcc678629d985453f115
sejal <sakharwsejal@gmail.com>
2026-04-25 18:37:34 +0530

    Web Medical Chatbot completed

--- src/helper.py
+++ src/helper.py
@@ -59,19 +59,19 @@ def text_split(minimal_docs):
 
 
 def download_embeddings():
-    # model_name = "sentence-transformers/all-mpnet-base-v2"
-    # model_kwargs = {"device": "cpu"}
-    # encode_kwargs = {"normalize_embeddings": False}
-    # embeddings= HuggingFaceEmbeddings(
-    #     model_name=model_name,
-    #     model_kwargs=model_kwargs,
-    #     encode_kwargs=encode_kwargs,
-    # )
-
-
-    embeddings = OllamaEmbeddings(
-        model="llama3.2",
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {"device": "cpu"}
+    encode_kwargs = {"normalize_embeddings": False}
+    embeddings= HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
     )
+
+
+    # embeddings = OllamaEmbeddings(
+    #     model="llama3.2",
+    # )
 
     return embeddings
 
